c7fa79c222985b13b811fb3d7c7ddfc224de7b52
aloix123 <pawel.kupis@o2.pl>
2023-08-17 09:16:54 +0200

    Initial commit

---
+++ src/test/java/com/mainpackage/PasswordManager/PasswordManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mainpackage.PasswordManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
