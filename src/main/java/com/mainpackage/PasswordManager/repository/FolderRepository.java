a54116fbbfe33397c590fab39fbd233a35ab7a52
kupisos <pawel.kupis@o2.pl>
2024-07-07 15:24:58 +0200

    cut old models end repos . Create new diagrams done!!

--- src/main/java/com/mainpackage/PasswordManager/repository/FolderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mainpackage.PasswordManager.repository;
-
-import com.mainpackage.PasswordManager.model.Folder;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FolderRepository extends CrudRepository<Folder,Long> {
-}
--- src/main/java/com/mainpackage/PasswordManager/repository/PassowrdHintRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mainpackage.PasswordManager.repository;
-
-import com.mainpackage.PasswordManager.model.PasswordHint;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PassowrdHintRepository extends CrudRepository<PasswordHint,Long> {
-}
