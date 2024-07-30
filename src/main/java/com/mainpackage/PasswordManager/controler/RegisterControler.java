e8782efe33066d831a04177c48fabd07d9d91b92
kupisos <pawel.kupis@o2.pl>
2024-07-30 10:15:55 +0200

    default folder and safe done!!1

--- src/main/java/com/mainpackage/PasswordManager/controler/RegisterControler.java
+++ src/main/java/com/mainpackage/PasswordManager/controler/RegisterControler.java
@@ -1,10 +1,16 @@
 package com.mainpackage.PasswordManager.controler;
 
 import com.mainpackage.PasswordManager.Action.EncryptDataClass;
+import com.mainpackage.PasswordManager.Util.UserUtil;
 import com.mainpackage.PasswordManager.Util.UserUtilService;
 import com.mainpackage.PasswordManager.model.CustomUser;
+import com.mainpackage.PasswordManager.model.Folder;
+import com.mainpackage.PasswordManager.model.Safe;
+import com.mainpackage.PasswordManager.repository.CustomUserRepository;
 import com.mainpackage.PasswordManager.service.CustomUserServiceImp;
+import com.mainpackage.PasswordManager.service.FolderService;
 import com.mainpackage.PasswordManager.service.RegisterService;
+import com.mainpackage.PasswordManager.service.SafeServiceImp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,8 +24,12 @@ public class RegisterControler {
     private RegisterService registerService;
     @Autowired
     private CustomUserServiceImp customUserServiceImp;
+
     UserUtilService userUtilService=new UserUtilService();
     EncryptDataClass encryptDataClass =new EncryptDataClass();
+    @Autowired
+    private CustomUserRepository customUserRepository;
+
 
     @PostMapping("/getStarted")
     public ResponseEntity<String> ShowRegisterInterface (@RequestBody CustomUser customUser) throws Exception {
@@ -33,9 +43,38 @@ public class RegisterControler {
             return new ResponseEntity<>("there is  an account with the same email",HttpStatus.BAD_REQUEST);
         }
         else{
-            userUtilService.SetUserParameters(customUser);
-            registerService.saveUser(customUser);
+            createUser(customUser);
             return new ResponseEntity<>(" you created a user!!", HttpStatus.OK);
         }
     }
+
+    @Autowired
+    private SafeServiceImp safeServiceImp;
+    @Autowired
+    private FolderService folderService;
+
+    public void createDefaultUserAssets(Long id){
+        String defaultSafeName="main";
+        Safe defaultSafe=new Safe();
+        defaultSafe.setUserid(id);
+        defaultSafe.setName(defaultSafeName);
+        safeServiceImp.createSafe(defaultSafe);
+        String defaultFolderName="none";
+        Folder defaultFolder=new Folder();
+        defaultFolder.setSafeid(defaultSafe.getSafeid());
+        defaultFolder.setName(defaultFolderName);
+        folderService.save(defaultFolder);
+    }
+
+    public void  createUser(CustomUser customUser){
+        CustomUser user=new CustomUser();
+        customUser.setUserid(user.getUserid());
+        registerService.saveUser(customUser);
+        userUtilService.SetUserParameters(customUser);
+        Long id=UserUtil.getId();
+        System.out.println(id);
+        createDefaultUserAssets(id);
+
+    }
+
 }
